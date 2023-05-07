5c891dc5f62bb535b0e2bfbbbaa345d464b78738
Mahmoud Gamal <mahgam3396@gmail.com>
2023-05-08 01:52:39 +0300

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# upload-to-google-storage-bash-script
+# Upload to Google Storage Bash Script
+
+This bash script allows you to upload a file to Google Cloud Storage using a Service Account JSON key file.
+
+## Prerequisites
+
+- A Google Cloud Platform project with a Cloud Storage bucket
+- A Service Account with the necessary permissions and a JSON key file
+- curl and openssl installed on your system
+
+## Usage
+
+1. Save the bash script as `upload_to_google_storage.sh`.
+
+2. Replace the `json_file` variable value with the path to your Service Account JSON key file:
+
+   ```bash
+   json_file="googlestorage_credentials_json_file.json"
+   
+3. Replace the UPLOAD_ARCHIVE variable value with the path to the file you want to upload:
+   
+   UPLOAD_ARCHIVE="path_to_your_file_to_upload"
+
+4. Make the script executable:
+   chmod +x upload_to_google_storage.sh
+
+5. Run the script:
+   ./upload_to_google_storage.sh
+
+   The script will output the HTTP status code indicating the success or failure of the upload. If the upload is successful, you will see a "200" status code.
+
+##Troubleshooting
+If you encounter any issues during the execution, double-check the following:
+
+	. Ensure you have the correct path to your Service Account JSON key file
+	. Verify that the Service Account has the necessary permissions to upload to the specified Cloud Storage bucket
+	. Make sure your file path is correct and the file exists
+	
+If you continue to have issues, inspect the API response or response code for more information on the error.
---
+++ upload-to-google-storage-bash-script.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+echo "UPLOAD TO GOOGLE STORAGE SCRIPT RUNNING ..."
+
+# Add the required data from your JSON key file
+json_file="googlestorage_credintials_json_file.json"
+
+TOKEN_URI=$(awk -F'"' '/"token_uri":/{print $(NF-1)}' "$json_file")
+CLIENT_EMAIL=$(awk -F'"' '/"client_email":/{print $(NF-1)}' "$json_file")
+PRIVATE_KEY=$(awk -F'"' '/"private_key":/{print $(NF-1)}' "$json_file")
+
+if [ ! -e "${_path}/tmp_private_key.pem" ]; then
+  printf "%b" "$PRIVATE_KEY" > "${_path}/tmp_private_key.pem"
+fi
+
+# Create JWT header
+header=$(echo -n '{"alg":"RS256","typ":"JWT"}' | openssl base64 -A | tr -d '\n=' | tr '+/' '-_')
+
+# Create JWT claim set
+now=$(date +%s)
+claim_set=$(printf '{"iss":"%s","scope":"https://www.googleapis.com/auth/devstorage.full_control","aud":"%s","exp":%d,"iat":%d}' "$CLIENT_EMAIL" "$TOKEN_URI" $(($now + 3600)) $now)
+claim_set=$(echo -n "$claim_set" | openssl base64 -A | tr -d '\n=' | tr '+/' '-_')
+
+# Sign the JWT
+signature=$(echo -n "$header.$claim_set" | openssl dgst -sha256 -sign ${_path}/tmp_private_key.pem | openssl base64 -A | tr -d '\n=' | tr '+/' '-_')
+
+# Create the signed JWT
+jwt="${header}.${claim_set}.${signature}"
+
+# Request the access token
+API_RESPONSE=$(curl -s -d "grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion=$jwt" "$TOKEN_URI")
+echo "API response: $API_RESPONSE"
+
+# Extract the access token from the API response
+ACCESS_TOKEN=$(echo "$API_RESPONSE" | sed -n 's/.*"access_token" *: *"\([^"]*\)".*/\1/p')
+
+UPLOAD_ARCHIVE="path_to_your_file_to_upload"
+
+# #### End of configuration variables
+
+echo "Sending..." $UPLOAD_ARCHIVE
+
+# Upload to Google Storage
+upload_to_google_storage() {
+  local bucket_name="fx30-logfile-uploads" # Replace with your bucket name
+  local file_path="$1"
+  local file_name="$(basename "$file_path")"
+  local access_token="$2"
+
+  response=$(curl -s -o /dev/null -w '%{http_code}' -X POST -L "https://storage.googleapis.com/upload/storage/v1/b/${bucket_name}/o?uploadType=multipart" \
+    -H "Authorization: Bearer $access_token" \
+    -F "metadata={\"name\": \"$file_name\"};type=application/json;charset=UTF-8" \
+    -F "file=@$file_path;type=application/octet-stream")
+
+  echo "$response"
+}
+
+response_code=$(upload_to_google_storage "$UPLOAD_ARCHIVE" "$ACCESS_TOKEN")
+echo "response_code: $response_code"
+
+if [ "$response_code" -eq 200 ]; then
+  echo "Upload successful"
+else
+  echo "Upload failed with status code: $response_code"
+fi
+
+rc=$?
+if test "$rc" != "0"; then
+  echo "Send failed with return code: $rc"
+else
+  echo "OK"
+fi
+
